58210aa947fb91986f50d192591b9a68ccb7c44f
minobel <129174583+minobel@users.noreply.github.com>
2024-10-26 02:57:36 +0600

    Create Best_Time_to_Buy_and_Sell _tock.cpp

---
+++ Week-01/Best_Time_to_Buy_and_Sell _tock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int  mini_price = prices[0];
+        int max_profit = 0;
+        for( int i = 1; i < n; i++){
+            mini_price = min( mini_price , prices[i]);
+            int profit = prices[i] - mini_price;
+            max_profit = max(profit , max_profit);
+        }
+        return max_profit;
+    }
+};
