9691b6b397232b75059a0ae7b536bd4ae6035fb7
minobel <minobel2000@gmail.com>
2025-08-02 02:17:51 +0600

    Create 523. Continuous Subarray Sum.cpp

---
+++ Week-04(Prefix_Sum)/523. Continuous Subarray Sum.cpp
@@ -0,0 +1,21 @@
+//TC - O(N)
+//SC - O(N)
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> rem_idx;
+        rem_idx[0] = -1;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if (rem_idx.find(sum % k) == rem_idx.end()) {
+                rem_idx[sum % k] = i;
+            }
+            if (rem_idx.find(sum % k) != rem_idx.end() &&
+                (i - rem_idx[sum % k]) >= 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
