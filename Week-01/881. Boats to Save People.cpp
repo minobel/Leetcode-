bf2af57ccade7072153120b038881d12b39e402b
minobel <minobel2000@gmail.com>
2025-08-08 23:42:32 +0600

    Solved Boat Rescued Problem

---
+++ Week-01/881. Boats to Save People.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+    int count = 0;
+    int i = 0; 
+    int j = people.size() - 1;
+    sort(people.begin(), people.end());
+    while( i <= j ){
+        if((people[i] + people[j]) <= limit){
+            count++;
+            i++;
+            j--;
+        }
+        else{
+            count++;
+            j--;
+        }
+    }
+    return count;
+    }
+};
